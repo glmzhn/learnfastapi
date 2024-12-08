8f423080ac4c0fceb5b1711a97a95afd4d7f6345
glmzhn <galymzhan2007@gmail.com>
2024-12-08 23:04:39 +0500

    Second

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional, Dict
 
 app = FastAPI()
 
+data = [
+    {'id': 1, 'title': 'News 1', 'text': 'Text 1'},
+    {'id': 2, 'title': 'News 2', 'text': 'Text 2'},
+    {'id': 3, 'title': 'News 3', 'text': 'Text 3'}
+]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+class Item(BaseModel):
+    id: int
+    title: str
+    text: str
+
+
+@app.get("/items")
+async def get_items() -> List[Item]:
+    return [Item(**post) for post in data]
+
+
+@app.get("/items/{id}")
+async def get_item(id: int) -> Item:
+    for post in data:
+        if post['id'] == id:
+            return Item(**post)
+
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get("/search")
+async def search(post_id: Optional[int] = None) -> Dict[str, Optional[Item]]:
+    if post_id:
+        for post in data:
+            if post['id'] == post_id:
+                return {'data': Item(**post)}
+
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    else:
+        return {'data': None}
